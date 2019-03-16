eb3b5fd3bcae0e91bba9d31f33d653870c6e7931
Lulu <luluwheelan@gmail.com>
2019-03-16 11:50:58 -0400

    Add project files.

---
+++ Group7A2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Group7A2.MvcApplication" Language="C#" %>
---
+++ Group7A2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group7A2.Startup))]
+namespace Group7A2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
